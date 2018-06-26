079da16fec9ce65d64ef0d3741dd17e8e02be40e
Arief Eko <riff.sense@gmail.com>
2018-06-27 00:28:26 +0700

    add migration

---
+++ app/migrations/1.0.0/user.php
@@ -0,0 +1,84 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class UserMigration_100
+ */
+class UserMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('user', [
+                'columns' => [
+                    new Column(
+                        'id_user',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'nama_user',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 100,
+                            'after' => 'id_user'
+                        ]
+                    ),
+                    new Column(
+                        'email_user',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 100,
+                            'after' => 'nama_user'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id_user'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '4',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'latin1_swedish_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
